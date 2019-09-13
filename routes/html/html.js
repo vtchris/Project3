ffbb0ac134b46f3827187dbf2ba3a7a0bccd543c
VTC <vtchris@hotmail.com>
2019-09-13 17:11:57 -0400

    attempt to correct prod error

--- routes/html/html.js
+++ routes/html/html.js
@@ -10,6 +10,9 @@ router.get("/enroll", function(req, res) {
     //res.sendFile(path.join(__dirname, "../../client/src/index.js"));
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
   });
+  router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+  }
  
 
   module.exports = router;
